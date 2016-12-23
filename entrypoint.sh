5a6323e85264955d29341568c32f4c362212e0ee
malaohu <re_ming.ma@hotmail.com>
2016-12-23 17:02:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 #nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 #ping www.baidu.com -c 5
 
-pm2 start /wwwroot/server.js
+node /wwwroot/server.js
 
 #/usr/bin/python /ssr/shadowsocks/server.py "$@"
