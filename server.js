19d32b9e49a229cf6c54fd2a9699402f864971da
malaohu <re_ming.ma@hotmail.com>
2016-12-25 00:57:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
+var express = require('express');
 var superagent = require('superagent');
 
-require('http').createServer(function(req, res) {  
+var app = express()
+
+app.get('/', function (req, res) {
     superagent.post("https://app.arukas.io/api/login")
-    .send({email: "$ENAIL", password: "$PWD"})
-    .end((err, sres) => {
-        var cookie = sres.header['set-cookie'];
-        superagent.get("https://app.arukas.io/api/containers")
-        .set("Cookie", cookie)
-        .end((err, sres) => {
-            var t = sres.text;
-            var i = t.indexOf('"container_port":1234');
-            var port = t.slice(i+37, i+42);
-            var ip = t.slice(i+57, t.indexOf(".jp", i+57)).replace(/-/g, '.');
-            res.write("ip :  " + ip + "\n");
-            res.write("port: " + port + "\n");
-            res.write("pass: 1024\n");
-            res.end("crypto: aes-256-cfb\n");
-        });
-    });
-}).listen(1080);
+        .send({email: "yourmail@gmail.com", password: "yourArukasPassword"})
+        .end(function(lres){
+             var cookie = lres.header['set-cookie'];
+            superagent.get("https://app.arukas.io/api/containers")
+            .set("Cookie", cookie)
+            .end(function(sres){
+                res.send(sres.body);
+            })
+        })
+})
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80')
+})
