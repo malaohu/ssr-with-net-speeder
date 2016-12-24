f6bdcc71ddb28af86223f2fcaf7e654a39759820
malaohu <re_ming.ma@hotmail.com>
2016-12-25 01:04:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.get('/', function (req, res) {
         })
 })
 
+app.get('/i', function (req, res) {
+    res.send('http://51.ruyo.net');
+})
+
+app.get('/a', function (req, res) {
+    res.send({email: "$EMAIL", password: "$PWD"});
+})
+
 app.listen(80, function () {
   console.log('Example app listening on port 80')
 })
