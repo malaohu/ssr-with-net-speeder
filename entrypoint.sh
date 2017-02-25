02b8c3d518bb18da590c77a01aae27aaadeb1fde
juzeon <812312770@qq.com>
2017-02-25 13:55:27 +0800

    fix ssh problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "-Dockerfile by juzeon"
 echo "-root password: password"
-/usr/sbin/sshd restart
+/etc/init.d/ssh restart
 echo "-sshd started"
 nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 echo "-net_speeder started"
