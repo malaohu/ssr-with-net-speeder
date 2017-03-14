4665298796a429cf30190a2a0d7049ec9ccd0041
snquentin <snquentin@msn.com>
2017-03-14 16:30:44 +0800

    返回

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 ping www.baidu.com -c 5
 
 
-/usr/bin/python /shadowsocksr/shadowsocks/server.py "$@"
+/usr/bin/python /ssr/shadowsocks/server.py "$@"
