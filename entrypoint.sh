c1921057f4205389eec7f6551e28cb490416740e
malaohu <re_ming.ma@hotmail.com>
2017-04-17 17:15:31 +0800

    启动net speeder时判断网卡名称

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+#start net speeder
+ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
+if [ "$ETH"  ==  '1' ] ; then
+  nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+fi
+if [ "$ETH"  ==  '0' ] ; then
+  nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
+fi
 
 /usr/bin/python /ssr/shadowsocks/server.py "$@"
